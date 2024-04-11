97fe8b0f4bc8888255bcd7de68eed73ffccb462c
Sagar Puneria <sagar.puneria@gmail.com>
2024-04-11 22:28:42 +0530

    root commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FormsModule } from '@angular/forms';
+import { ColorDirective } from './directives/color.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    ColorDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
